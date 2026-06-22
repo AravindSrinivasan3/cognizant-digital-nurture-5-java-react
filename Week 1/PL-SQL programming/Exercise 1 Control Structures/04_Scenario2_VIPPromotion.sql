f064d855d76b2a40f44ef4d057eab9e2e1165d06
Aravind Srinivasan <mailaravind3505@gmail.com>
2026-06-22 14:44:09 +0530

    Exercise 1: Implement PL/SQL control structures for banking scenarios

---
+++ Week 1/PL-SQL programming/Exercise 1 Control Structures/04_Scenario2_VIPPromotion.sql
@@ -0,0 +1,20 @@
+BEGIN
+    FOR cust IN (
+        SELECT CustomerID
+        FROM Customers
+        WHERE Balance > 10000
+    )
+    LOOP
+        UPDATE Customers
+        SET IsVIP = 'TRUE'
+        WHERE CustomerID = cust.CustomerID;
+
+        DBMS_OUTPUT.PUT_LINE(
+            'Customer promoted to VIP: '
+            || cust.CustomerID
+        );
+    END LOOP;
+
+    COMMIT;
+END;
+/
